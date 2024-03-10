d932bc7836b7b16ab895c325a839b06fa9bc7c18
twinkocat <msbulahmaxim@gmail.com>
2024-03-10 09:57:59 +0200

    [ADD] LoadingPrefab

---
+++ Assets/Scripts/Core/Utilities/UISpriteAnimation.cs
@@ -0,0 +1,76 @@
+﻿// file UISpriteAnimation.cs created by twinkocat
+// 
+// (c) 2024 twinkocat. All rights reserved.
+
+#region
+
+using UnityEngine;
+using UnityEngine.UI;
+
+#endregion
+
+namespace twinkocat.Core.Utilities
+{
+    [RequireComponent(typeof(Image))]
+    public class UISpriteAnimation : MonoBehaviour
+    {
+        [SerializeField] private bool     _isRevertable;
+        [SerializeField] private int      _fps;
+        [SerializeField] private Sprite[] _sprites;
+
+        private float _delta;
+        private Image _image;
+        private int   _index;
+        private float _timer;
+        private bool  _isReversing;
+        private void Awake() => _image = GetComponent<Image>();
+
+        private void Animation(float deltaTime)
+        {
+            if (_fps <= 0 || _sprites.IsNullOrEmpty()) return; 
+            
+            _timer += deltaTime;
+       
+            var interval = 1f / _fps;
+        
+            if (_timer < interval) return;
+            
+            _timer -= interval;
+            _index = _isRevertable ? ReverseAnimation() : DefaultAnimation();
+            _image.sprite = _sprites[_index];
+        }
+
+        private int DefaultAnimation() => (_index + 1) % _sprites.Length;
+
+        private int ReverseAnimation()
+        {
+            if (_isReversing)
+            {
+                _index--;
+
+                if (_index >= 0) return _index;
+                
+                _index = 1;
+                _isReversing = false;
+            }
+            else
+            {
+                _index++;
+
+                if (_index < _sprites.Length) return _index;
+                
+                _index = _sprites.Length - 2; 
+                _isReversing = true;
+            }
+
+            return _index;
+        }
+
+        private void Update()
+        {
+            if (!enabled) return;
+            
+            Animation(Time.deltaTime);
+        }
+    }
+}
