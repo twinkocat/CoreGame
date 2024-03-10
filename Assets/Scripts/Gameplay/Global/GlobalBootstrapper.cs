3609b70952e1172db49a1bdf206cdcd32b4b5831
twinkocat <msbulahmaxim@gmail.com>
2024-03-10 09:07:22 +0200

    [ADD] Base GameFlow

---
+++ Assets/Scripts/Gameplay/Global/GlobalBootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using twinkocat.Core.Bootstrap;
+using twinkocat.Core.Services.Interfaces;
+using twinkocat.Gameplay.Global.Services;
+
+namespace twinkocat.Gameplay.Global
+{
+    public class GlobalBootstrapper : Bootstrapper
+    {
+        public override void RegisterServices(IServiceRegister serviceRegister)
+        {
+            serviceRegister.RegisterService<ApplicationLoadService>(this);
+        }
+    }
+}
