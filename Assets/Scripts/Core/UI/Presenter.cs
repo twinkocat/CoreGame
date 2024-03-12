ae89c2214164a9a97004849af5c0edf1831190ca
twinkocat <msbulahmaxim@gmail.com>
2024-03-12 08:58:53 +0200

    [ADD] Base UI abstractions

---
+++ Assets/Scripts/Core/UI/Presenter.cs
@@ -0,0 +1,16 @@
+﻿using twinkocat.Core.UI.Interfaces;
+
+namespace twinkocat.Core.UI
+{
+    public abstract class Presenter<TView> : IPresenter where TView : IView
+    {
+        protected TView View;
+    }
+    
+    public abstract class Presenter<TView, TModel> : Presenter<TView> 
+        where TView  : IView 
+        where TModel : IModel
+    {
+        protected TModel Data;
+    }
+}
---
+++ Assets/Scripts/Core/UI/View.cs
@@ -0,0 +1,16 @@
+﻿// file View.cs created by twinkocat
+// 
+// (c) 2024 twinkocat. All rights reserved.
+
+using twinkocat.Core.UI.Interfaces;
+using UnityEngine;
+
+namespace twinkocat.Core.UI
+{
+    public abstract class View<T> : IView where T : MonoBehaviour
+    {
+        protected T ViewComponent;
+
+        protected View(T viewComponent) => ViewComponent = viewComponent;
+    }
+}
